a1f92f961ed919c9303928a5b0f356fe16d77ff8
sowish23 <soso2266@naver.com>
2020-11-13 17:43:43 +0900

    build custom user model

--- drf_study/admin.py
+++ drf_study/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nickname',
+        'email',
+        'date_joined',
+    )
+
+    list_display_links = (
+        'nickname',
+        'email',
+    )
--- drf_study/views.py
+++ drf_study/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from drf_study.serializers import UserSerializer, GroupSerializer
-
+from .models import User
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    model = User
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
