6d62c10e982668b82c473f8afd971b3ffdd8c7bc
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2020-11-15 01:17:07 +0900

    add custom user model and admin

--- drf_study/models.py
+++ drf_study/models.py
@@ -1,3 +1,52 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# BaseUserMananger : User를 생성할 떄 사용하는 클래스
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('must have user email')
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+# 곤라지 User 생성하는 함수
+    def create_superuser(self, email, nickname, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(
+        max_length=20,
+        null=False,
+        unique=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'nickname'
+    REQUIRED_FIELDS = ['email']
